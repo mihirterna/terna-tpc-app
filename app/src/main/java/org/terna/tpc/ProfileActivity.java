613cff143e1d99808af5f51b230195bb3d20e11b
dhaneshkatre <katre.dhanesh26@gmail.com>
2017-08-27 02:58:09 +0530

    Refactored and added ProfileActivity

--- app/src/main/java/org/terna/tpc/ProfileActivity.java
+++ app/src/main/java/org/terna/tpc/ProfileActivity.java
@@ -2,6 +2,7 @@ package org.terna.tpc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,6 +13,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -24,16 +28,28 @@ public class ProfileActivity extends AppCompatActivity {
         FirebaseUser user = firebaseAuth.getCurrentUser();
         final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Students").child(user.getUid());
 
-        final TextView profileView = (TextView)findViewById(R.id.profileView);
+        final ImageView profileImageView = (ImageView)findViewById(R.id.user_profile_photo);
+        final TextView profileNameView = (TextView)findViewById(R.id.user_profile_name);
+        final TextView profileEmailView = (TextView)findViewById(R.id.user_profile_email);
+        final TextView profileDobView = (TextView)findViewById(R.id.user_profile_dob);
+        final TextView profileStationView = (TextView)findViewById(R.id.user_profile_city);
+        final TextView profileMarksView = (TextView)findViewById(R.id.user_profile_marks);
+        final TextView profileExtrasView = (TextView)findViewById(R.id.user_profile_extras);
+
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 try {
                     Information receivedInfo = dataSnapshot.getValue(Information.class);
-                    profileView.setText(receivedInfo.getEmail() +"\n"+ receivedInfo.getName());
+                    HashMap<String,String> receivedAcademics = (HashMap<String,String>)dataSnapshot.child("Academics & Extras").getValue();
+                    profileNameView.setText(receivedInfo.getName());
+                    profileEmailView.setText(receivedInfo.getEmail());
+                    profileDobView.setText(receivedInfo.getDob());
+                    profileStationView.setText(receivedInfo.getCity());
+                    profileMarksView.setText("FE: "+receivedAcademics.get("FE")+"\nSE: "+receivedAcademics.get("SE")+"\nTE: "+receivedAcademics.get("TE"));
+                    profileExtrasView.setText(receivedAcademics.get("EXTRAS"));
                 }catch(Exception r){
-                    Toast.makeText(getApplicationContext(),r.getMessage(),Toast.LENGTH_LONG).show();
-                    finish();
+                    r.printStackTrace();
                 }
             }
 
